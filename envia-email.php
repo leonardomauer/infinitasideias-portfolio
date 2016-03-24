784b8aa4deae75b6c23f67f2fd87f89b69ce4562
Leonardo Mauer <leo.mauer@gmail.com>
2016-03-24 17:12:23 -0300

    import inicial

---
+++ envia-email.php
@@ -0,0 +1,20 @@
+﻿<?php
+ 
+$email_servidor = "contato@infinitasideias.com.br";
+$para = "contato@infinitasideias.com.br";
+$email = $_POST['email'];
+$mensagem = $_POST['message'];
+$nome = $_POST['nome'];
+$assunto = "Contato do Site - ".$nome;
+
+function enviaEmail($email, $assunto, $mensagem, $para, $email_servidor) {
+    $headers = "From: $email_servidor\r\n" .
+               "Reply-To: $email\r\n" .
+               "X-Mailer: PHP/" . phpversion() . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+  
+  mail($para, $assunto, nl2br($mensagem), $headers);
+}
+ header("Location: ".$_SERVER['HTTP_REFERER']."");
+?>
